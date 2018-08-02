c7e06b984f3b1acf6409c88fcf38fff9c0fd50d4
Ahmed Bentajer <a.bentajer@gmail.com>
2018-08-02 10:29:43 +0100

    fix travis errors

--- src/test/java/com/mmfive/test/AppsTest.java
+++ src/test/java/com/mmfive/test/AppsTest.java
@@ -3,18 +3,20 @@ package com.mmfive.test;
 import com.mmfive.Mmcinq;
 import com.mmfive.api.ApiClient;
 import com.mmfive.exceptions.ApiCallFailedException;
-import com.mmfive.responses.TestResponse;
-import org.junit.Test;
-
+import com.mmfive.responses.StartResponse;
+import junit.framework.TestCase;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 import java.net.MalformedURLException;
 
-import static org.junit.Assert.assertEquals;
 
-public class AppsTest {
+public class AppsTest extends TestCase {
 
     static String CODE = "123456789";
     static int MAX_CODE_DIGIT = CODE.length();
     static int MAX_NUMBER = 10; // from 0 to 9
+
+
 /*
     @Test
     public void testFindNumberInCode(){
@@ -28,21 +30,40 @@ public class AppsTest {
         String result = Mmcinq.replaceCharAt("12345",1,(char) 3);
         assertEquals("13345" ,result);
     }
-*/
+
     @Test
     public void searchNumberAtIndex() throws ApiCallFailedException, MalformedURLException {
 //        TestResponse tr = new TestResponse(0,0 );
 //        int[] t = new int[]{1,2,6,8,0};
 //        int n = Mmcinq.searchNumberAtIndex(1,"12345", t , tr,
 //                new ApiClient("http://172.16.37.129/api/", "token", "tokenmm5", 30000, 3000));
-        assertEquals(2,2);
+        Assertions.assertEquals(2,2);
+    }*/
+
+    @Test
+    public void generateCodeFromTable() throws MalformedURLException, ApiCallFailedException {
+
+        try {
+            System.out.println("Testing schedule retrieval: OK");
+
+            try {
+                startResponse = apiClient.getStartResponse();
+                System.out.println(startResponse.toString());
+            } catch (ApiCallFailedException ex) {
+                System.out.println("Already started");
+                startResponse = new StartResponse(8,"mm5",123456);
+                System.out.println(startResponse.toString());
+        StartResponse sr = null;
+        ApiClient apiClient = new ApiClient("http://172.16.37.129/api/", "token", "tokenmm5", 30000, 3000);
+
+        sr =  new ApiClient("http://172.16.37.129/api/", "token", "tokenmm5", 30000, 3000).getStartResponse();
+        int[] t = Mmcinq.findNumbersInCode( new ApiClient("http://172.16.37.129/api/", "token", "tokenmm5", 30000, 3000)
+        ,sr);//new int[]{0,1,1,2,2,2,3,3,3,3,4,4,4,4,4,5,5,5,5,5,5,6,6,6,6,6,6,6,7,7,7,7,7,7,7,7,8,8,8,8,8,8,8,8,8};
+        String result = Mmcinq.generateCodeFromTable(t);
+        System.out.println("Printint result : "+result);
+        Assertions.assertEquals(t,result);
     }
 /*
-    @Test
-    public void generateCodeFromTable(int[] resfindNumbersInCode){
-        fail("test generate code from table not implemented yet");
-    }
-
     @Test
     public void sortNumbersInCode(int[] resfindNumbersInCode){
         fail("test sort number in code not implemented yet");
--- src/test/java/com/mmfive/test/TestRunner.java
+++ src/test/java/com/mmfive/test/TestRunner.java
@@ -4,7 +4,7 @@ import org.junit.runner.JUnitCore;
 import org.junit.runner.Result;
 import org.junit.runner.notification.Failure;
 
-public class TestRunner {
+public class TestRunner extends junit.textui.TestRunner  {
 
     public static void main(String args[]) {
         Result result = JUnitCore.runClasses(AppsTest.class);
