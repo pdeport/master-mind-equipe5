42dbc6c2d5a32cdc5b20c42dece4a81492b5bfa3
Ahmed Bentajer <a.bentajer@gmail.com>
2018-08-02 11:04:52 +0100

    test last function

--- src/test/java/com/mmfive/test/AppsTest.java
+++ src/test/java/com/mmfive/test/AppsTest.java
@@ -42,31 +42,22 @@ public class AppsTest extends TestCase {
 
     @Test
     public void generateCodeFromTable() throws MalformedURLException, ApiCallFailedException {
-
-        try {
-            System.out.println("Testing schedule retrieval: OK");
-
-            try {
-                startResponse = apiClient.getStartResponse();
-                System.out.println(startResponse.toString());
-            } catch (ApiCallFailedException ex) {
-                System.out.println("Already started");
-                startResponse = new StartResponse(8,"mm5",123456);
-                System.out.println(startResponse.toString());
         StartResponse sr = null;
         ApiClient apiClient = new ApiClient("http://172.16.37.129/api/", "token", "tokenmm5", 30000, 3000);
-
+        sr = apiClient.getStartResponse();
         sr =  new ApiClient("http://172.16.37.129/api/", "token", "tokenmm5", 30000, 3000).getStartResponse();
-        int[] t = Mmcinq.findNumbersInCode( new ApiClient("http://172.16.37.129/api/", "token", "tokenmm5", 30000, 3000)
-        ,sr);//new int[]{0,1,1,2,2,2,3,3,3,3,4,4,4,4,4,5,5,5,5,5,5,6,6,6,6,6,6,6,7,7,7,7,7,7,7,7,8,8,8,8,8,8,8,8,8};
+        int[] t = Mmcinq.findNumbersInCode( apiClient, sr);//new int[]{0,1,1,2,2,2,3,3,3,3,4,4,4,4,4,5,5,5,5,5,5,6,6,6,6,6,6,6,7,7,7,7,7,7,7,7,8,8,8,8,8,8,8,8,8};
         String result = Mmcinq.generateCodeFromTable(t);
         System.out.println("Printint result : "+result);
         Assertions.assertEquals(t,result);
     }
-/*
+
     @Test
-    public void sortNumbersInCode(int[] resfindNumbersInCode){
-        fail("test sort number in code not implemented yet");
+    public void sortNumbersInCode() throws MalformedURLException, ApiCallFailedException{
+        StartResponse sr = null;
+        ApiClient apiClient = new ApiClient("http://172.16.37.129/api/", "token", "tokenmm5", 30000, 3000);
+        sr = apiClient.getStartResponse();
+        sr =  new ApiClient("http://172.16.37.129/api/", "token", "tokenmm5", 30000, 3000).getStartResponse();
     }
-*/
+
 }
